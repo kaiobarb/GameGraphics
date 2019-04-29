cbb3bb837a18c0bae8316e7b31326fda81fea6bd
Kaio <kaiobarb@gmail.com>
2019-04-28 20:08:47 -0700

    downsizing

--- GameGraphics/Assets/RotateLight.cs
+++ GameGraphics/Assets/RotateLight.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class RotateLight : MonoBehaviour
 {
-    public Light light;
+    private Vector3 pivotPoint = new Vector3(0,0,0);
+
+    public float radius = 5;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pivotPoint = transform.position;
+        float newX = pivotPoint.x;
+        newX += radius;
+        transform.position = new Vector3(newX, pivotPoint.y, pivotPoint.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.RotateAround(pivotPoint, Vector3.up, 20 * Time.deltaTime);
     }
 }
